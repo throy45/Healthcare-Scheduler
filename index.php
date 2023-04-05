c5a1abdb4b5b18fdf6d1ffb880df57b9d5987ca5
throy45 <thomas.roy.2@ulaval.ca>
2023-04-05 11:06:50 -0400

    Changed index logic, added column name, added infected names

--- index.php
+++ index.php
@@ -9,10 +9,13 @@
 </head>
 <body>
     <h1>Welcome to HFESTS web app</h1>
-    <a href="./pages/Facilities/index.php">Facilities</a><br>
     <a href="./pages/Employees/index.php">Employees</a><br>
+    <a href="./pages/Facilities/index.php">Facilities</a><br>
+    <a href="./pages/Infections/index.php">Infections</a><br>
+    <a href="./pages/Manager/index.php">Manager</a><br>
+    <a href="./pages/Managers/index.php">Managers</a><br>
     <a href="./pages/PostalCodes/index.php">Postal Codes</a><br>
-    <a href="./pages/Vaccines/index.php">Vaccines</a><br>
     <a href="./pages/Schedule/index.php">Schedule</a><br>
+    <a href="./pages/Vaccines/index.php">Vaccines</a><br>
 </body>
 </html>
--- pages/Infections/index.php
+++ pages/Infections/index.php
@@ -1,7 +1,8 @@
-<?php require_once '../database.php';
-$statement = $conn->prepare('SELECT * FROM Infections');
-$statement->execute();
-mysqli_stmt_bind_result($statement, $row['EmployeeID'], $row['InfectionID'], $row['Type'], $row['Date']);
+<?php
+    require_once '../../database.php';
+    $statement = $conn->prepare('SELECT * FROM Infections, Employees WHERE Infections.EmployeeID = Employees.EmployeeID');
+    $statement->execute();
+    mysqli_stmt_bind_result($statement, $row['EmployeeID'], $row['InfectionID'], $row['Type'], $row['Date'], $row['EmployeeID2'], $row['FName'], $row['LName'], $row['Role'], $row['DateOfBirth'], $row['MedicareNumber'], $row['Email'], $row['Citizenship'], $row['PhoneNumber'], $row['Address'])
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +11,7 @@ mysqli_stmt_bind_result($statement, $row['EmployeeID'], $row['InfectionID'], $ro
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../pages/style.css">
-    <title>List of Infections</title>
+    <title>Infections</title>
 </head>
 <body>
     <h1>List of Infections</h1>
@@ -19,16 +20,19 @@ mysqli_stmt_bind_result($statement, $row['EmployeeID'], $row['InfectionID'], $ro
         <thead>
             <tr>
                 <th>Employee ID</th>
-                <th>Infection ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
                 <th>Type</th>
                 <th>Date</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
             <?php while (mysqli_stmt_fetch($statement)) {?>
                 <tr>
                     <td><?php echo $row['EmployeeID'] ?></td>
-                    <td><?php echo $row['InfectionID'] ?></td>
+                    <td><?php echo $row['FName'] ?></td>
+                    <td><?php echo $row['LName'] ?></td>
                     <td><?php echo $row['Type'] ?></td>
                     <td><?php echo $row['Date'] ?></td>
                     <td>
@@ -39,8 +43,6 @@ mysqli_stmt_bind_result($statement, $row['EmployeeID'], $row['InfectionID'], $ro
             <?php }; ?>
         </tbody>
     </table>
-
-    <a href="../">Back to main page</a>
-    
+    <a href="../../index.php">Back to main page</a>
 </body>
 </html>
